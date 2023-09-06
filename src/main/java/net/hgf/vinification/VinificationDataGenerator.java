9fb4ba0a7313c18a9dd097438f132aa07292d75d
HunterGhostfist <Hunter090403@Gmail.com>
2023-09-06 08:11:14 -0400

    Initial commit

---
+++ src/main/java/net/hgf/vinification/VinificationDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hgf.vinification;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VinificationDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/hgf/vinification/VinificationModClient.java
@@ -0,0 +1,10 @@
+package net.hgf.vinification;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VinificationModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
